b03f572d995d1770e8314b6998199dffc7ae1e8e
Julie <juliecomilla@gmail.com>
2023-08-12 21:11:45 -0400

    completed files

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+function writeCards(names, saying) {
+    let messages = []
+    for (let i =0; i < names.length ; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${saying} gift!`)
+    console.log(messages);
+
+}
+return messages; 
+//console.log(writeCards(names, "surprise"))
+}
+
+
+
+function countDown(number) {
+while (number >= 0) {
+    console.log(number--);
+}
+
+}
